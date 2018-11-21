5700561ed9771bb051c15246112204a579e75877
Anway <anwaysomani@gmail.com>
2018-11-21 14:12:05 +0530

    Added authentication features

---
+++ MiniMarketBill/src/java/bean/Provider.java
@@ -0,0 +1,9 @@
+package bean;
+
+public interface Provider {
+String DRIVER="oracle.jdbc.driver.OracleDriver";
+String CONNECTION_URL="jdbc:mysql://localhost:3306/billing_system";
+String USERNAME="Anway";
+String PASSWORD="anway123";
+
+}
